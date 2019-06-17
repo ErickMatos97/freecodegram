1d58f026114014a60e98e7d24f92ee4aebd1383c
Erick Matos <erick.matos@upr.edu>
2019-06-17 11:41:25 -0400

    Finished course.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
@@ -12,14 +13,36 @@ class ProfilesController extends Controller
     {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
 
-        return view('profiles.index', compact('user' , 'follows'));
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->posts->count();
+            });
+
+        $followersCount = Cache::remember(
+            'count.followers.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->profile->followers->count();
+            });
+
+
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->following->count();
+            });
+
+        return view('profiles.index', compact('user', 'follows', 'postCount', 'followersCount', 'followingCount'));
 
     }
 
     public function edit(User $user)
     {
         $this->authorize('update', $user->profile);
-      return view('profiles.edit' , compact('user'));
+        return view('profiles.edit', compact('user'));
 
     }
 
@@ -34,14 +57,13 @@ class ProfilesController extends Controller
         ]);
 
 
+        if (request('image')) {
+            $imagePath = (request('image')->store('profile', 'public'));
 
-      if (request('image')) {
-          $imagePath = (request('image')->store('profile', 'public'));
-
-          $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
-          $image->save();
-          $imageArray = ['image' => $imagePath];
-      }
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+            $imageArray = ['image' => $imagePath];
+        }
 
         auth()->user()->profile->update(array_merge(
             $data,
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,23 +9,31 @@
                         <img src="/storage/{{$post->image}}" class="w-100">
                     </a>
                 </div>
-                <div class="row pt-2 pb-4">
-                    <div class="col-6 offset-3">
-                        <div class="d-flex align-items-baseline">
+            </div>
+            <div class="row pt-2 pb-4">
+                <div class="col-6 offset-3">
 
-                            <p>
+
+                    <p>
                         <span class="font-weight-bold">
                        <a href="/profile/{{$post->user->id}}">
                            <span class="text-dark">{{$post->user->username}}
                            </span>
-                       </a> {{$post->caption}}
-                            </p>
+                       </a> </span>
+                        {{$post->caption}}
+                    </p>
 
-                        </div>
-                    </div>
-                    @endforeach
                 </div>
             </div>
+        @endforeach
+
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center">
+                {{ $posts->links() }}
+            </div>
+        </div>
+    </div>
+    </div>
     </div>
 
 @endsection
